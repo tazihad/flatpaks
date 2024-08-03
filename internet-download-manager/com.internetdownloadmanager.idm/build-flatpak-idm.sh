1622dfc8af85086f2836fed0e3ea179515a21a54
zihad <tazihad@gmail.com>
2024-08-04 01:24:39 +0600

    updated idm

--- internet-download-manager/com.internetdownloadmanager.idm/build-flatpak-idm.sh
+++ internet-download-manager/com.internetdownloadmanager.idm/build-flatpak-idm.sh
@@ -5,15 +5,15 @@ if [ -f "./idm.flatpak" ]; then rm ./idm.flatpak; fi
 
 echo "Building with flatpak builder..."
 flatpak run org.flatpak.Builder --force-clean idm com.internetdownloadmanager.idm.yml >/dev/null
-echo "✔️ Done"
+echo "✓ Done"
 
 echo "Running flatpak build-export..."
 flatpak build-export export idm >/dev/null
-echo "✔️ Done"
+echo "✓ Done"
 
 echo "Running flatpak build-bundle..."
 flatpak build-bundle export idm.flatpak com.internetdownloadmanager.idm >/dev/null
-echo "✔️ Done"
+echo "✓ Done"
 
 
 
@@ -22,5 +22,5 @@ echo "Cleanup..."
 if [ -d "./.flatpak-builder" ]; then rm -rf ./.flatpak-builder; fi
 if [ -d "./export" ]; then rm -rf ./export; fi
 if [ -d "./idm" ]; then rm -rf ./idm; fi
-echo "✔️ Done"
+echo "✓ Done"
 
--- internet-download-manager/com.internetdownloadmanager.idm/include/idm.sh
+++ internet-download-manager/com.internetdownloadmanager.idm/include/idm.sh
@@ -3,16 +3,17 @@ export WINEDLLPATH=/app/dlls/lib32:/app/dlls/lib
 export WINEPREFIX=/var/data/wine
 export WINE="/app/bin/wine"
 export WINE_C_DRIVE="${WINEPREFIX}/drive_c"
-export WINBOX_DOWNLOAD_URL='https://mirror2.internetdownloadmanager.com/idman642build18.exe?v=lt&filename=idman642build18.exe'
+export IDM_DOWNLOAD_URL='https://download.filepuma.com/files/download-managers/internet-download-manager/Internet_Download_Manager_v6.36%20Build%205.exe'
 export IDM_LOCATION="${WINE_C_DRIVE}/Program Files (x86)/Internet Download Manager/IDMan.exe"
-export TEMP="${WINE_C_DRIVE}/windows/temp"
+USER_NAME=$(whoami)
+export TEMP_DIR="${WINE_C_DRIVE}/users/${USER_NAME}/Temp"
 
 echo "Checking if Internet Download Manager is installed..."
 if [ ! -f "${IDM_LOCATION}" ]; then
-    echo "Internet Download Manager not found. Installing latest version..."
-    wget -O "${TEMP}/idm.exe" "${WINBOX_DOWNLOAD_URL}"
-    "${WINE}" "${TEMP}/idm.exe"
+    echo -e "Internet Download Manager not found.\nInstalling best wine compatible version idman636build5..."
+    mkdir -p "${TEMP_DIR}"
+    curl -L "${IDM_DOWNLOAD_URL}" -o "${TEMP_DIR}/idm.exe"
+    "${WINE}" "${TEMP_DIR}/idm.exe"
 fi
-echo "\$1: '$1' \$2: '$2'"
 
 "${WINE}" "${IDM_LOCATION}"
